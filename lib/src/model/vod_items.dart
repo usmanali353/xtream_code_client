008f576d28a82aa307736f6ecf7331b9c7752d42
usmanali353 <aliusman353@gmail.com>
2025-09-04 20:52:18 +0500

    Update vod_items.dart
    added tmdbs id in vod model

--- lib/src/model/vod_items.dart
+++ lib/src/model/vod_items.dart
@@ -1,8 +1,8 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:xtream_code_client/src/utils/json_helper.dart';
-
+ 
 part 'vod_items.g.dart';
-
+ 
 /// Represents a VOD (Video On Demand) item in Xtream Code.
 @JsonSerializable()
 class XTremeCodeVodItem {
@@ -23,69 +23,73 @@ class XTremeCodeVodItem {
     required this.containerExtension,
     required this.customSid,
     required this.directSource,
+    required this.tmdbId,
   });
-
+ 
   /// Creates a new instance of [XTremeCodeVodItem] from a JSON object.
   factory XTremeCodeVodItem.fromJson(Map<String, dynamic> json) =>
       _$XTremeCodeVodItemFromJson(json);
-
+ 
   /// The ID of the stream of the VOD item.
   @JsonKey(name: 'stream_id', fromJson: dynamicToIntConverter)
   final int? streamId;
-
+ 
   /// The number of the VOD item.
   @JsonKey(fromJson: dynamicToIntConverter)
   final int? num;
-
+ 
   /// The name of the VOD item.
   final String? name;
-
+ 
   /// The title of the VOD item.
   final String? title;
-
+ 
   /// The year the VOD item was released.
   final String? year;
-
+ 
   /// The type of the stream of the VOD item.
   @JsonKey(name: 'stream_type')
   final String? streamType;
-
+ 
   /// The icon of the stream of the VOD item.
   @JsonKey(name: 'stream_icon')
   final String? streamIcon;
-
+ 
   /// The rating of the VOD item.
   @JsonKey(fromJson: dynamicToDoubleConverter)
   final double? rating;
-
+ 
   /// The rating of the VOD item based on a scale of 5.
   @JsonKey(name: 'rating_5based', fromJson: dynamicToDoubleConverter)
   final double? rating5based;
-
+ 
   /// The date the VOD item was added.
   @JsonKey(fromJson: dateTimeFromEpochSeconds)
   final DateTime? added;
-
+ 
   /// The ID of the category the VOD item belongs to.
   @JsonKey(name: 'category_id', fromJson: dynamicToIntConverter)
   final int? categoryId;
-
+ 
   /// The IDs of the categories the VOD item belongs to.
   @JsonKey(name: 'category_ids', fromJson: intListFromJson)
   final List<int>? categoryIds;
-
+ 
   /// The container extension of the VOD item.
   @JsonKey(name: 'container_extension')
   final String? containerExtension;
-
+ 
   /// The custom SID of the VOD item.
   @JsonKey(name: 'custom_sid')
   final String? customSid;
-
+ 
   /// The direct source of the VOD item.
   @JsonKey(name: 'direct_source')
   final String? directSource;
-
+ 
+  @JsonKey(name: 'tmdb', fromJson: dynamicToIntConverter)
+  final int? tmdbId;
+ 
   /// Converts this instance into a JSON object.
   Map<String, dynamic> toJson() => _$XTremeCodeVodItemToJson(this);
 }
